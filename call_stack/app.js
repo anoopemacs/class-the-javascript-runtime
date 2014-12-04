1007a91b282bc894374d50ade2a1e97335d4e961
Chris Mather <mather.chris@gmail.com>
2014-12-03 16:34:57 -0800

    add call_stack example

--- call_stack/app.js
+++ call_stack/app.js
@@ -1,29 +1,41 @@
-function three() {
-  console.log('function: three');
-  return 'three'
-}
+console.log('running our program');
+
+/**
+ * Example functions to put on the call stack.
+ */
 
 function two() {
-  console.log('function: two');
-  var threeReturnValue = three();
-  return 'two'
+  console.log('two');
+  return 2;
 }
 
 function one() {
-  console.log('function: one');
-  var twoReturnValue = two();
-  return 'one';
-}
-
-function onClickHandler(e) {
+  // stack overflow!
   debugger;
-  console.log('click event: ', e);
+  one();
 }
 
-var buttonElement = document.getElementById('myButton');
-buttonElement.addEventListener('click', onClickHandler);
+/**
+ * Example to illustrate some of what a lower level
+ * run time method might do in order to call a function.
+ */
+var heap = {};
+var stack = [];
 
-debugger;
-console.log('start');
-one();
-console.log('end');
+var callFunction = function (nameFunc, args, lineNumber) {
+  var func = heap[nameFunc];
+
+  var stackFrame = {
+    func: func,
+    name: nameFunc,
+    args: args,
+    env: {},
+    returnValue: undefined
+  };
+
+  stack.push(stack);
+
+  var returnValue = func.apply(this, args);
+  stackFrame.returnValue = returnValue;
+  return stack.pop();
+};
