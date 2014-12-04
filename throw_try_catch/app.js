75b163e368b2aa840e11b3846cd4aa8fd7236c7e
Chris Mather <mather.chris@gmail.com>
2014-12-03 17:39:43 -0800

    add some comments

--- throw_try_catch/app.js
+++ throw_try_catch/app.js
@@ -6,6 +6,10 @@ var MySpecialCode = {
 function six() {
   console.log('six');
   debugger;
+
+  // throw the MySpecialCode object down the
+  // call stack until the runtime finds a catch
+  // that knows how to deal with it.
   throw MySpecialCode;
   return;
 }
@@ -28,6 +32,10 @@ function three() {
   try {
     four();
   } catch (obj) {
+    // catch any thrown object, and if it's our MySpecialObject
+    // do something special, otherwise rethrow the object down
+    // the callstack. Notice, at this point, functions five and six
+    // have been skipped (we jumpted to this point in the call stack)!
     if (obj === MySpecialCode) {
       console.log(obj);
       debugger;
